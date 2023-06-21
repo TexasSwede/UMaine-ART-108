07da39566071fd2e8bcfb9230b5b827ecfcd543c
khm-wipfli <karl-henry.martinsson@wipfli.com>
2023-06-20 23:46:57 -0400

    Added popup welcome message, updated icons on main page, etc

--- public/index.js
+++ public/index.js
@@ -1,16 +1,28 @@
 
 $(window).ready(function () {
-    //alert("foo");
     setTimeout(function () {
-        $(".fade-in-on-load").fadeTo(750, 1);
+        $(".title-section").fadeTo(400, 1, function() {
+            $(".title-section .fade-in-on-load").fadeTo(350, 1);
+            $(".popup-window").fadeTo(750, 1);
+        });
+        $(".title-section .fade-in-on-load").fadeTo(750, 1);
+        $(".popup-window").fadeTo(750, 1);
     }, 1000);
 
     $(".wrapper-bulletpoint").on("click", function(){
-        playSound();
+        playSound("ding.mp3");
     });
+
+    $(".popup-close").on("click", function() {
+        playSound("ding.mp3");
+        $(".popup-window").fadeTo(500,0, function() {
+            $(".fade-in-on-load").fadeTo(750, 1);
+        });
+    });
+
 });
 
-function playSound () {
-	let snd = new Audio('./assets/ding.mp3');
+function playSound (file) {
+	let snd = new Audio(`./assets/${file}`);
 	snd.play();
 }
