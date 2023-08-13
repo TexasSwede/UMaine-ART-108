e1c5aa85465a24b43cde0628011951e40b489702
khm-wipfli <karl-henry.martinsson@wipfli.com>
2023-08-13 09:52:09 -0400

    Added sound

--- public/index.js
+++ public/index.js
@@ -1,5 +1,5 @@
 
-let currentImg = 0; 
+let currentImg = 0;
 const interval = 3000;
 const NAV_START = 0;
 const NAV_TOP = 1;
@@ -7,13 +7,16 @@ const NAV_TOP = 1;
 $(window).ready(function () {
     // Find all elements with class .hidden and hide them using jQuery instead
     let allHidden = $(".hidden");
-    allHidden.fadeOut(100, function(){
+    allHidden.fadeOut(100, function () {
         allHidden.removeClass("hidden");
     });
     $(".title-home-icon").hide();
-    
+
     // Wait 1 second more, to allow CSS to load/reformat elements
     setTimeout(function () {
+        var jqxhr = $.get("http://node-red.texasswede.com:1880/speak?text=Welcome to my portfolio website", function () {
+            //console.log("success");
+        });
         let titleSection = $(".title-section");
         fadeOnLoad = titleSection.find(".fade-in-on-load");
         // We can remove the class now, the elements should be hidden by code above
@@ -39,7 +42,7 @@ $(window).ready(function () {
             let nextPageTitle = nextPage.data("title");
             $("#title-text").html(nextPageTitle);
             // Set to transparent while still hidden, show it (still faded out), then fade in
-            nextPage.fadeIn(750, function(){
+            nextPage.fadeIn(750, function () {
                 //$(".title-home-icon").fadeIn(750);
             });
             $(".title-home-icon").fadeIn(1000);
@@ -61,7 +64,7 @@ $(window).ready(function () {
             let nextPageTitle = nextPage.data("title");
             $("#title-text").html(nextPageTitle);
             // Fade out while still hidden, then show it (still faded out), and then fade in
-            nextPage.fadeIn(750, function(){
+            nextPage.fadeIn(750, function () {
                 //navigationButtons(NAV_START);
             });
             navigationButtons(NAV_START);
@@ -81,12 +84,12 @@ $(window).ready(function () {
         });
     });
 
-    $(".campaign-navigation-button .dot").on("click", function(){
+    $(".campaign-navigation-button .dot").on("click", function () {
         let clicked = $(this).attr("id").slice(4);
         changeSlide(clicked);
     });
 
-    $("#logo").on("click", function(){
+    $("#logo").on("click", function () {
         var win = window.open('http://www.texasswede.com/', '_blank');
         if (win) {
             //Browser has allowed it to be opened
@@ -100,36 +103,36 @@ $(window).ready(function () {
 
 
 function navigationButtons(buttonPos) {
-    if (buttonPos==NAV_START) {
-        $('div.bulletpoint-icon.home').animate( {top: 0, left: 0}, "slow");
-        $('div.bulletpoint-icon.first').animate( {top: 80, left: -40}, "slow");
-        $('div.bulletpoint-icon.second').animate( {top: 147, left: -95}, "slow");
-        $('div.bulletpoint-icon.third').animate( {top: 214, left: -149}, "slow");
-        $('div.bulletpoint-icon.forth').animate( {top: 281, left: -200}, "slow");
-        $('div.bulletpoint-icon.fifth').animate( {top: 348, left: -255}, "slow");
-    } else if (buttonPos==NAV_TOP) {
-        $('div.bulletpoint-icon.home').animate( {top: 39, left: -10}, "slow");
-        $('div.bulletpoint-icon.first').animate( {top: 0, left: 0}, "slow");
-        $('div.bulletpoint-icon.second').animate( {top: 0, left: 0}, "slow");
-        $('div.bulletpoint-icon.third').animate( {top: 0, left: 0}, "slow");
-        $('div.bulletpoint-icon.forth').animate( {top: 0, left: 0}, "slow");
-        $('div.bulletpoint-icon.fifth').animate( {top: 0, left: 0}, "slow");
+    if (buttonPos == NAV_START) {
+        $('div.bulletpoint-icon.home').animate({ top: 0, left: 0 }, "slow");
+        $('div.bulletpoint-icon.first').animate({ top: 80, left: -40 }, "slow");
+        $('div.bulletpoint-icon.second').animate({ top: 147, left: -95 }, "slow");
+        $('div.bulletpoint-icon.third').animate({ top: 214, left: -149 }, "slow");
+        $('div.bulletpoint-icon.forth').animate({ top: 281, left: -200 }, "slow");
+        $('div.bulletpoint-icon.fifth').animate({ top: 348, left: -255 }, "slow");
+    } else if (buttonPos == NAV_TOP) {
+        $('div.bulletpoint-icon.home').animate({ top: 39, left: -10 }, "slow");
+        $('div.bulletpoint-icon.first').animate({ top: 0, left: 0 }, "slow");
+        $('div.bulletpoint-icon.second').animate({ top: 0, left: 0 }, "slow");
+        $('div.bulletpoint-icon.third').animate({ top: 0, left: 0 }, "slow");
+        $('div.bulletpoint-icon.forth').animate({ top: 0, left: 0 }, "slow");
+        $('div.bulletpoint-icon.fifth').animate({ top: 0, left: 0 }, "slow");
     }
 }
 
 function playSound(file) {
     let snd = new Audio(`./assets/${file}`);
     snd.play();
-} 
+}
 
 function changeSlide(n) {
     let imgs = $('.campaign-slider img');
     let dots = $('.dot');
     imgs.hide();
     dots.removeClass("active")
-  
+
     currentImg = n;
-  
+
     $(`#img-${n}`).show();
     $(`#dot-${n}`).addClass(' active');
-  }
+}
